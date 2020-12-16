d64f7b2747e790ab17b6a77eccb3e39d0160b70b
Nova Pratiwi Damayanti <58509273+novapratiwi@users.noreply.github.com>
2020-12-16 14:39:23 +0800

    login, logout, transaksi

---
+++ PROJECT PBO UAS/main.py
@@ -0,0 +1,138 @@
+import datetime
+import sqlite3
+import pembukuan as p
+databaseName = "pembukuan.db"
+conn = sqlite3.connect(databaseName)
+
+print("===SISTEM PEMBUKUAN===\n1.Masukan Data Karyawan Baru\n2.Login\n")
+pilihan = int(input("masukan pilihan anda : "))
+
+if pilihan == 1:
+    conn.execute(
+    "CREATE TABLE IF NOT EXISTS Karyawan (username text primary key, jabatan text, email text, name text, address text, phoneNumber text, password text)"
+    )
+
+    inputKaryawan = p.Karyawan(input("Username :"),input("Email :"),input("Nama :"),input("Alamat :"),input("Nomor Telepon :"),input("Password :"))
+
+    res = conn.execute("select * from Karyawan where username = ?", (inputKaryawan.get_username(),))
+
+    cursor = conn.cursor().execute("select * from Karyawan")
+    for row in cursor:
+        if res.fetchone() is None:
+            conn.execute("insert into Karyawan values (?,?,?,?,?,?,?)", (inputKaryawan.get_username(),inputKaryawan.get_jabatan(),inputKaryawan.get_email(), inputKaryawan.get_name(), inputKaryawan.get_address(), inputKaryawan.get_phoneNumber(), inputKaryawan.get_password()))
+            conn.commit()
+            print("data karyawan berhasil ditambahkan")
+        else :
+            print("maaf username yang anda masukan sudah terdaftar")
+
+elif pilihan == 2:
+    conn.execute(
+    "CREATE TABLE IF NOT EXISTS Login(username text primary key, password text, status text)"
+    )
+
+    login = p.Login(input("Username :"), input("Password :"))
+
+    username = login.get_username()
+    password = login.get_password()
+
+    cursor = conn.cursor().execute("select username,password from Karyawan")
+    
+    for row in cursor:
+        print(row[0])
+        masuk = False
+        if username == row[0] and password == row[1]:
+            conn.execute("insert into Login values (?,?,?)", (login.get_username(), login.get_password(), login.get_status()))
+            conn.commit()
+            print("anda berhasil login")
+            masuk = True
+            break
+        elif username == row[0] and password != row[1]:
+            print("maaf password yang anda masukan salah")
+            break
+        elif username != row[0] and password == row[1]:
+            print("maaf username yang anda masukan salah")
+            break
+        else :
+            print("username dan password yang anda masukan salah")
+            break
+
+    if masuk == True:
+        print("1. LOG OUT\n2. StokBrg\n3. Transaksi")
+        choose = int(input("Masukan pilihanmu :"))
+
+        if choose == 1:
+            conn.execute("delete from Login where username = ?", (username,))
+            conn.commit()
+            print("anda berhasil log out")
+        
+        if choose == 2:
+            conn.execute(
+                "CREATE TABLE IF NOT EXISTS Stock_Barang (idBarang int primary key,namaBarang text,hargaSatuan int, persediaan int, jumlahTerjual int)"
+                )
+            
+            tambahstok = p.Stock_Barang(input("id barang : "), input("nama barang : "), input("harga : "), input("persediaan"), input("jumlah terjual : "))
+            
+            conn.execute("insert into Stock_Barang values (?,?,?,?,?)", (tambahstok.get_idBarang(), tambahstok.get_namaBarang(), tambahstok.get_harga(), tambahstok.get_persediaan(), tambahstok.get_jumlahTerjual()))
+
+
+        if choose == 3:
+            conn.execute(
+                "CREATE TABLE IF NOT EXISTS Transaksi (idTransaksi int primary key, username text, namaPembeli text, idBarang text, listBarang text, hargaSatuan text, totalHarga int, waktuTransaksi DATETIME)"
+                )
+            
+            listbrg = []
+
+            transaksi = p.Transaksi(input("nama pembeli : "),input("nama barang : "))
+            listbrg.append(transaksi.get_listBarang())
+            lanjut = input("lagi? (y/n) : ")
+            while lanjut == "y":
+                more = input("nama barang : ")
+                listbrg.append(more)
+                lanjut = input("lagi? (y/n) : ")
+                if lanjut == "n":
+                    break
+            #menjadikan list id barang menjadi string
+            print(listbrg)
+            newlistbrg = ",".join(listbrg)
+
+
+            #hitung baris di db transaksi
+            cursor1 = conn.cursor().execute("select idTransaksi from Transaksi")
+            for row in cursor1:
+                transaksi.get_noTransaksi += row
+
+            #mengambil idbarang dari tabel stock brg
+            cursor2 = conn.cursor().execute("select * from Stock_Barang")
+            for row in cursor2:
+                for i in listbrg:
+                    if row[1] == i:
+                        transaksi.idBarang.append(row[0])
+                        transaksi.hargaSatuan.append(row[2])
+                        transaksi.totalHarga += (row[2])
+
+            tanggal = datetime.datetime.now()
+
+            #menjadikan list idbarang menjadi string
+            newlistIDbarang = ",".join(map(str, transaksi.idBarang))
+
+            #menjadikan list harga satuan menjadi string
+            newlistHargaSatuan = ",".join(map(str, transaksi.hargaSatuan))
+
+
+            
+            conn.execute("insert into Transaksi values (?,?,?,?,?,?,?,?)", (transaksi.get_noTransaksi(), login.get_username(), transaksi.get_namaPembeli(), newlistIDbarang, newlistbrg, newlistHargaSatuan, transaksi.totalHarga, tanggal))
+
+           
+        conn.execute("delete from Login where username = ?", (username,))
+        conn.commit()
+        print("anda berhasil log out")
+
+    
+
+
+# membaca data lewat python
+# cursor = conn.cursor().execute("select * from Karyawan")
+# for row in cursor:
+#     print(row)
+
+conn.close()
---
+++ PROJECT PBO UAS/pembukuan.py
@@ -0,0 +1,132 @@
+class Login:
+    def __init__(self,username, password):
+        self.__username = username
+        self.__password = password
+        self.__status = "online"
+    
+    
+    def get_username(self):
+        return self.__username
+
+    def get_password(self):
+        return self.__password
+    
+    def set_username(self, new):
+        self.__username = new
+
+    def set_password(self,new):
+        self.__password = new
+
+    def get_status(self):
+        return self.__status
+
+class User(Login):
+    def __init__(self, username, email, nama, alamat, nomorTlpn, password):
+        super().__init__(username, password)
+        self.__email = email
+        self.__name = nama
+        self.__address = alamat
+        self.__phoneNumber = nomorTlpn
+
+    def get_email(self):
+        return self.__email
+    
+    def get_name(self):
+        return self.__name
+    
+    def get_address(self):
+        return self.__address
+    
+    def get_phoneNumber(self):
+        return self.__phoneNumber
+    
+    def set_email(self,new):
+        self.__email = new
+
+    def set_name(self,new):
+        self.__name = new
+
+    def set_address(self,new):
+        self.__address = new
+
+    def set_phoneNumber(self,new):
+        self.__phoneNumber = new
+
+
+
+class Owner(User):
+    def __init__(self, username, email, nama, alamat, nomorTlpn, password):
+        super().__init__(username, email, nama, alamat, nomorTlpn, password)
+        self.__jabatan = "owner"
+
+    def get_jabatan(self):
+        return self.__jabatan
+
+class Karyawan(User):
+    def __init__(self, username, email, nama, alamat, nomorTlpn, password):
+        super().__init__(username, email, nama, alamat, nomorTlpn, password)
+        self.__jabatan = "karyawan"
+
+    def get_jabatan(self):
+        return self.__jabatan
+
+class Stock_Barang:
+    def __init__(self, idbrg, namabrg, harga, persediaan, jumlahTerjual):
+        self.__idBarang = idbrg
+        self.__namaBarang = namabrg
+        self.__harga = harga
+        self.__persediaan = persediaan
+        self.__jumlahTerjual = jumlahTerjual
+
+    def get_idBarang(self):
+        return self.__idBarang
+    
+    def get_namaBarang(self):
+        return self.__namaBarang
+    
+    def get_harga(self):
+        return self.__harga
+    
+    def get_persediaan(self):
+        return self.__persediaan
+    
+    def get_jumlahTerjual(self):
+        return self.__jumlahTerjual
+
+class Transaksi(Login):
+    def __init__(self, namaPembeli, listBarang):
+        self.__noTransaksi = 1
+        self.__namaPembeli = namaPembeli
+        self.idBarang = []
+        self.__listBarang = listBarang
+        self.hargaSatuan = []
+        self.totalHarga = 0
+        self.__tanggalPembelian = 0
+
+    def get_noTransaksi(self):
+        return self.__noTransaksi
+
+    def get_listBarang(self):
+        return self.__listBarang
+
+    def get_namaPembeli(self):
+        return self.__namaPembeli
+
+    def get_tanggalPembelian(self):
+        return self.__tanggalPembelian
+    
+
+
+    
+
+# karyawan1 = Karyawan(input("a :"),input("b :"),input("c :"),input("d :"),input("e :"),input("f :"))
+# print(karyawan1.__dict__)
+
+# kar1 = Login("adinda1", "abcdefg")
+# print(kar1.__dict__)
+# print(kar1.get_status())
+
+# kar1.log_out()
+
+# print(kar1.__dict__)
+# print(kar1.get_status())
